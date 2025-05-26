36fdfdb69ab5d07e319528bb261cf8600ae79e3f
Nurdin <mrsunnysummer@gmail.com>
2025-05-27 03:38:12 +0700

    feat: add splash screen

---
+++ app/src/main/java/com/nudriin/someproject/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.nudriin.someproject
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nudriin.someproject.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupView()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val splashIntent = Intent(this, MainActivity::class.java)
+            splashIntent.flags =
+                Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(splashIntent)
+            finish()
+        }, 3000)
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+
+}

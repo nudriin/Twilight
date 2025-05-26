99f84ae1c8b2f680f43e3aaffe930ad0d07e334c
Nurdin <mrsunnysummer@gmail.com>
2025-05-27 02:26:47 +0700

    feat: create user adapter and activate view binding

--- app/src/main/java/com/nudriin/someproject/adapter/UserAdapter.kt
+++ app/src/main/java/com/nudriin/someproject/adapter/UserAdapter.kt
@@ -1,4 +1,35 @@
 package com.nudriin.someproject.adapter
 
-class UserAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nudriin.someproject.data.UserData
+import com.nudriin.someproject.databinding.CardUserBinding
+
+class UserAdapter(private val cardList: List<UserData>) :
+    RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = CardUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = cardList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val card = cardList[position]
+        holder.bind(card)
+    }
+
+    class ViewHolder(private val binding: CardUserBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: UserData) {
+            with(binding) {
+                tvName.text = data.name
+                tvEmail.text = data.email
+                tvPhone.text = data.phone
+            }
+        }
+    }
+
 }

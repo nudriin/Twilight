91a10e318d9a0dfe155fa6abcf28dd203689f2c4
Nurdin <mrsunnysummer@gmail.com>
2025-05-27 02:50:09 +0700

    feat: complete ui and login in activity main and fix compileSdk and targetSdk in gradle

--- app/src/main/java/com/nudriin/someproject/MainActivity.kt
+++ app/src/main/java/com/nudriin/someproject/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nudriin.someproject.adapter.UserAdapter
+import com.nudriin.someproject.data.UserData
+import com.nudriin.someproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupView()
+    }
+
+    private fun setupView() {
+        val data = listOf<UserData>(
+            UserData("Ragil", "ragil@gmail.com", "088128384"),
+            UserData("Nurdin", "nudriin@gmail.com", "08812384"),
+            UserData("FyodoR", "fyodoR@gmail.com", "088128384"),
+            UserData("Rosemary", "rM@gmail.com", "088128384"),
+            UserData("Ouros", "boros@gmail.com", "088128384"),
+        )
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvCardList.layoutManager = layoutManager
+        setUserData(data)
+    }
+
+    private fun setUserData(data: List<UserData>) {
+        val adapter = UserAdapter(data)
+        binding.rvCardList.adapter = adapter
     }
 }

9510d7507d44767900eb5213e10e707ba87bf60f
Nurdin <mrsunnysummer@gmail.com>
2025-05-28 04:53:06 +0700

    feat: create DescriptionFragment content

--- app/src/main/java/com/nudriin/someproject/ui/DescriptionFragment.kt
+++ app/src/main/java/com/nudriin/someproject/ui/DescriptionFragment.kt
@@ -1,18 +1,34 @@
 package com.nudriin.someproject.ui
 
+import android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import com.nudriin.someproject.R
+import com.nudriin.someproject.databinding.FragmentDescriptionBinding
+import com.nudriin.someproject.databinding.FragmentDetailProductBinding
 
 class DescriptionFragment : Fragment() {
+    private var _binding: FragmentDescriptionBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_description, container, false)
+    ): View {
+        _binding = FragmentDescriptionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.tvParagraph.justificationMode = JUSTIFICATION_MODE_INTER_WORD
+        }
     }
 }
